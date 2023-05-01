ec5bace2b990ce6260c408a6914ba39b275bf1e0
mneedham01 <mneedham@smith.edu>
2023-04-30 21:25:13 -0400

    added start of conversation method

--- Kitchen.java
+++ Kitchen.java
@@ -5,4 +5,18 @@ public class Kitchen extends Room{
     public Kitchen(String name, String description){
         super(name, description);
     }
+
+    public void conversation(ArrayList<String> wordList){
+        //inherit from Room
+        super.conversation(wordList);
+        //we are addressing an item 
+        //if (this.addressing!=null){
+            //if it's the first in the items list (TABLE)
+           // if(this.addressing==items.get(0)){
+                //print options 
+        //        // this.addressing.showOptions();
+        //     }
+        // }
+
+    }
 }
