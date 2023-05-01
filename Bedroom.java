c5dd30bcba89627fb110554b864c39175593e813
mneedham01 <mneedham@smith.edu>
2023-05-01 13:06:56 -0400

    added start of take blanket

--- Bedroom.java
+++ Bedroom.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Bedroom extends Room{
     
     String name;
@@ -5,5 +7,25 @@ public class Bedroom extends Room{
 
     public Bedroom(String name, String description){
         super(name, description);
+    }
+
+    public void conversation(ArrayList<String> wordList){
+        //inherit from Room
+        super.conversation(wordList);
+        //find first two words
+        String word1=wordList.get(0);
+        String word2=wordList.get(1);
+        //turn second word into an item
+        Item item2=this.returnItem(word2);
+
+        if (word1.equals("take")||word1.equals("Take")){
+            //if you have climbed on top of the bed 
+            //if(Game.climbedOn=bed){}
+                //if the item you're trying to take is the blanket 
+                if (item2.equals(items.get(1))){
+                    //Game.changeHolding(items.get(1));
+                }
+        }
+
     }
 }
