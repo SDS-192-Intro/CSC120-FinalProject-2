4f3f4804f9eefbcbf941c9a7732c06020f146fda
mneedham01 <mneedham@smith.edu>
2023-05-03 13:50:55 -0400

    delete unnecessary

--- Garden.java
+++ Garden.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 
 public class Garden extends Room {
 
@@ -7,12 +6,6 @@ public class Garden extends Room {
     
     public Garden(String name, String description,Game game){
         super(name, description,game);
-    }
-    
-    //Override for pond (change wording)
-    public void conversation(ArrayList<String> wordList){
-        try{
-        } catch (Exception e){}
     }
 
 }
