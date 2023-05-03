af3e89974df4d4ab88354a37981ee2fc15eb55d0
mneedham01 <mneedham@smith.edu>
2023-05-03 13:50:33 -0400

    deleted unnecessary stuff

--- Kitchen.java
+++ Kitchen.java
@@ -1,15 +1,7 @@
-import java.util.ArrayList;
 
 public class Kitchen extends Room{
     
-    public Kitchen(String name, String description,Game game){
+    public Kitchen(String name, String description, Game game){
         super(name, description,game);
-    }
-
-    public void conversation(ArrayList<String> wordList){
-        //inherit from Room
-        super.conversation(wordList);
-        
-
     }
 }
