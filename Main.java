2711305705013a7602e87fe359958c640bc1e453
mneedham01 <mneedham@smith.edu>
2023-05-05 11:14:59 -0400

    deleted print statements

--- Main.java
+++ Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         Game game=new Game();
-        System.out.println(game.timeConvert(203));
 
-        //game.play();
+        game.play();
 
     }
 }
