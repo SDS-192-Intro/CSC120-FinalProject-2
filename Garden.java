b8e987bcd2197b10a3066d2ee67965ba65ac826d
mneedham01 <mneedham@smith.edu>
2023-05-08 13:00:25 -0400

    added javadoc

--- Garden.java
+++ Garden.java
@@ -1,9 +1,9 @@
-
+/*Garden class extends Room */
 public class Garden extends Room {
 
     String name;
     String description;
-    
+    /* Garden constructor */
     public Garden(String name, String description,Game game){
         super(name, description,game);
     }
