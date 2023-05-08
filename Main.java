c2a225e030f67ee4fd4e7d3a03539401b9cc2a2c
mneedham01 <mneedham@smith.edu>
2023-05-08 12:59:38 -0400

    added javadoc

--- Main.java
+++ Main.java
@@ -1,4 +1,10 @@
+  /*
+ * Main class to run the game 
+    */
 public class Main {
+    /*
+     * Main method to start the game 
+     */
     public static void main(String[] args) {
         Game game=new Game();
 
