fbd5d5d15ab0d0dff5bf675c9f70fd43853e5f71
mneedham01 <mneedham@smith.edu>
2023-05-08 13:00:06 -0400

    added javadoc

--- Kitchen.java
+++ Kitchen.java
@@ -1,6 +1,7 @@
-
+/* Kitchen class extends room class  */
 public class Kitchen extends Room{
     
+    /* Kitchen constructor  */
     public Kitchen(String name, String description, Game game){
         super(name, description,game);
     }
